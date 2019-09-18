5bf0a438a6e8bbc2847d68028e665ff1d4f84229
lyle dickie <lyle.e.dickie@gmail.com>
2019-09-18 21:30:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    if not self.roster[grade]
+      self.roster[grade] = []
+      self.roster[grade] << student
+    else
+      self.roster[grade] << student
+    end
+  end
+  
+  def grade(grade_level)
+    # self.roster[grade]
+  end
+  
+end
